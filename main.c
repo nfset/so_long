672171a13668f9416f12ecc21d888d908899f716
Albert Pieniak <mojo.pl.006@gmail.com>
2025-02-26 18:27:16 +0100

    update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: apieniak <apieniak@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 13:14:53 by apieniak          #+#    #+#             */
-/*   Updated: 2025/02/25 15:18:05 by apieniak         ###   ########.fr       */
+/*   Updated: 2025/02/26 18:17:33 by apieniak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,29 @@
 
 typedef struct s_game
 {
-    void *main_mlx;
-	void *main_window;
+    void *mlx;
+	void *main_win;
     void *key;
+	char **map;
     int window_height;
     int window_width;
 
 }  t_game;
 
-// int		main_window_keys(void *key, void *mlx, void *main_window);
-// {
-// 	key = t_game->key;
-// 	if (key == 0xFF1B)
-// 		mlx_destroy_window(mlx, main_window);
-// }
-
-void	*create_main_window(void *mlx)
+int	exit_game(t_game *game)
 {
-	void	*window;
+	mlx_destroy_window(game->mlx, game->main_win);
+	free(game);
+	exit(EXIT_SUCCESS);
+}
 
-	window = mlx_new_window(mlx, 800, 700, "Main Window");
-	return (window);
-
+int		main_window_keys(int key, t_game *game)
+{
+	if (key == 0xFF1B)
+		exit_game(game);
+	else if (key == 0xFF52)
+		exit_game(game);
+	return (0);
 }
 
 int	main(void)
@@ -46,15 +47,15 @@ int	main(void)
 	game = malloc(sizeof(t_game));
 	if (!game)
 		return (1);
-	game->main_mlx = mlx_init();
-	if (!game->main_mlx)
+	game->mlx = mlx_init();
+	if (!game->mlx)
 	{
 		free(game);
 		return (1);
 	}
-	game->main_window = create_main_window(game->main_mlx);
-	mlx_loop(game->main_mlx);
+	game->main_win = mlx_new_window(game->mlx, 800, 700, "Main Window");
+	mlx_key_hook(game->main_win, main_window_keys, game);
+	mlx_loop(game->mlx);
 
-	free(game);
 	return (0);
 }
